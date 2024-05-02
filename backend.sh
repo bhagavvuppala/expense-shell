4528d2213437291d28b4e9878998c042eaf9ddbd
bhargav <vuppalabhargav99@gmail.com>
2024-05-02 15:24:42 +0530

    created shell script for frontend

--- backend.sh
+++ backend.sh
@@ -1,19 +1,22 @@
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
-dnf install nodejs -
+
+dnf install nodejs - y
+cp backened.service /etc/systemd/system/backend.service
+
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 pwd
 cd /app
 
-pwd
 unzip /tmp/backend.zip
 
 npm install
-cp backened.service /etc/systemd/system/backend.service
+
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
+
 dnf install mysql -y
 mysql -h 172.31.27.20 -uroot -pExpenseApp@1 < /app/schema/backend.sql
